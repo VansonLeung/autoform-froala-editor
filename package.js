aa7bbaaf53268d5e4589c707484eae335def23d2
root <clawfinga@gmail.com>
2015-12-29 05:06:13 -0500

    update

--- package.js
+++ package.js
@@ -1,20 +1,17 @@
 Package.describe({
   name: 'alexche:autoform-froala-editor',
   version: '0.0.4',
-  // Brief, one-line summary of the package.
-  summary: 'Froala editor for aldeed:autoform with S3 image uploader',
-  // URL to the Git repository containing the source code for this package.
+  summary: 'Froala V2 WYSIWYG editor for autoform with templating ability and S3 image upload',
   git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
+Npm.depends({ "busboy": "0.2.9" });
 
 Package.onUse(function(api) {
-  api.use(['lepozepo:s3','froala:editor@2.0.5','templating',
-    'aldeed:autoform','iron:router'],['client', 'server']);
+  api.use(['lepozepo:s3@5.1.5','froala:editor@2.0.1_1','templating@1.0.0',
+    'aldeed:autoform@5.7.1','iron:router@1.0.12'],['client', 'server']);
   api.addFiles(['afFroalaEditor.html','afFroalaEditor.js'],'client');
-  api.addFiles(['imageUpload.js'],'server');
+  api.addFiles(['imageUpload.js'],'server'); // there is a bug with Router.onBeforeAction
 });
 
 Package.onTest(function(api) {
