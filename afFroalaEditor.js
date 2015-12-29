b3b08ed534a6160ee5750eab26e36c43a3f6ce20
root <clawfinga@gmail.com>
2015-12-29 05:00:59 -0500

    update

--- afFroalaEditor.js
+++ afFroalaEditor.js
@@ -1,14 +1,13 @@
 AutoForm.addInputType('froalaEditor', {
     template:"afFroalaEditor",
     valueOut: function(){
-        return $(this).editable('getHTML', true, true);
+        return $(this).froalaEditor('html.get', true, true);
     }
 });
 
 Template.afFroalaEditor.helpers({
     atts: function(){
         var atts = _.clone(this.atts);
-
         // Remove froalaOptions so doesn't get rendered as html attrib
         delete atts.froalaOptions;
         return atts
@@ -16,66 +15,50 @@ Template.afFroalaEditor.helpers({
 });
 
 Template.afFroalaEditor.onRendered(function(){
-    var id = this.data.atts['data-schema-key'];
+    var id = this.data.atts.id;
     var afDropdownOptions = this.data.atts.froalaOptions;
     var froala_skel = {
         inlineMode: false,
-        buttons: ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'color', 'formatBlock', 'blockStyle', 'inlineStyle', 'align', 'insertOrderedList', 'insertUnorderedList', 'outdent', 'indent', 'selectAll', 'createLink', 'insertImage', 'insertVideo', 'table', 'undo', 'redo', 'html', 'insertHorizontalRule', 'uploadFile', 'removeFormat', 'fullscreen'],
-        customDropdowns: {},
-        imageUploadURL: '/upload_image_froala'
+        buttons: [],
+        customDropdowns: {}
     }
-
     // Assign basic editor variables
-    if(afDropdownOptions.height)
-        froala_skel.height = afDropdownOptions.height;
-    if(afDropdownOptions.inlineMode)
-        froala_skel.inlineMode = afDropdownOptions.inlineMode;
-    if(afDropdownOptions.buttons)
-        froala_skel.buttons = afDropdownOptions.buttons;
+    froala_skel.height = afDropdownOptions.height;
+    froala_skel.inlineMode = afDropdownOptions.inlineMode;
+    froala_skel.toolbarButtons = afDropdownOptions.buttons;
+    froala_skel.toolbarButtonsMD = afDropdownOptions.buttons;
+    froala_skel.toolbarButtonsXS = afDropdownOptions.buttons;
     if(afDropdownOptions.imageUploadURL)
         froala_skel.imageUploadURL=afDropdownOptions.imageUploadURL
-
     // Make sure all dropdowns are added to buttons
-    if(afDropdownOptions.customDropdowns) {
-        $.each(afDropdownOptions.customDropdowns, function (key, val) {
-            if (froala_skel.buttons.indexOf(key) == -1)
-                froala_skel.buttons.push(key);
-        });
-
-        // Assign dropdowns
-        froala_skel.customDropdowns = afDropdownOptions.customDropdowns;
-
-        // Convert each of the dropdowns options from string to function format
-        froala_skel.customDropdowns = convertDropdowns(froala_skel.customDropdowns);
-    }
-
-    // Enable editor using converted skeleton data
-    if($('[data-schema-key='+id+"]").data('fa.editable')) {
-        $('[data-schema-key='+id+"]").editable('destroy');
-    }
-    $('[data-schema-key='+id+"]").editable(froala_skel);
-});
-
-
-// Converts options params from strings
-// to functions
-function convertDropdowns(dropdowns){
-    Object.keys(dropdowns).forEach(function(val){
-        dropdowns[val].options = convertOptions(dropdowns[val].options);
+    $.each(afDropdownOptions.customDropdowns,function(key, val){
+        if(froala_skel.toolbarButtons.indexOf(key)==-1)
+            froala_skel.toolbarButtons.push(key);
     });
-    return dropdowns;
-}
-
-function convertOptions(options){
-    var newOptions = Object.keys(options).reduce(function ( obj, option ) {
-        if(typeof options[ option ] == 'string')
-            obj[ option ] = function () {
-                this.insertHTML(options[ option ]);
-            };
-        else
-            obj[ option ]=options[ option ]
-        return obj;
-    }, {});
-    return newOptions;
-}
-
+    // Assign dropdowns
+    froala_skel.customDropdowns = afDropdownOptions.customDropdowns;
+    // Convert each of the dropdowns options from string to function format
+    _.each(afDropdownOptions.customDropdowns, function(dd, name)
+    {
+        if(!dd.inverted)
+            dd.options = _.invert(dd.options)
+        dd.inverted = true
+        $.FroalaEditor.DefineIcon(name, {NAME: "lg "+dd.icon.value});
+        $.FroalaEditor.RegisterCommand(name, {
+            title: dd.title,
+            type: 'dropdown',
+            focus: false,
+            undo: false,
+            refreshAfterCallback: true,
+            options: dd.options,
+            callback: function (cmd, val) {
+                this.html.insert(val);
+            }
+        });
+    })
+    // Enable editor using converted skeleton data
+    if ($('#' + id).data('fa.editable')) {
+        $('#' + id).froalaEditor('destroy');
+    }
+    $('#' + id).froalaEditor(froala_skel);
+});
